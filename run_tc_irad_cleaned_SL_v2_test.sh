047dfa24b5cdf7767a110adfa3e5f280e691f572
Fabien <fabien.augsburger@unil.ch>
2024-04-16 11:34:00 +0200

    still not wotking to send slurm jobs at once

--- run_tc_irad_cleaned_SL_v2_test.sh
+++ run_tc_irad_cleaned_SL_v2_test.sh
@@ -20,19 +20,17 @@ source ~/.bashrc
 conda activate kera_lgbm
 
 # Define your array
-#SBATCH --array=0-2%2  # Change this to match the number of folders you have and the desired stride
+#SBATCH --array=1-10  # Change this to match the number of folders you have and the desired stride
 
-# Define your folders array
-folders=(
-    "10m_u_component_of_wind"
-    "10m_v_component_of_wind"
-)
+# Specify the path to the config file
+config=/work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/config_test.txt
+#echo "${SLURM_ARRAY_TASK_ID}"
 
-# Get the folder for this task
-folder="${folders[$SLURM_ARRAY_TASK_ID]}"
+# Extract the Nom_dossier for the current $SLURM_ARRAY_TASK_ID
+Nom_dossier=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 
-# Loop over years
-for year in {1990..1993}
-do
-    python3 /work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/tc_irad_multi_cleaned_SL.py "$folder" "$year"
-done
+# Extract the Annee for the current $SLURM_ARRAY_TASK_ID
+Annee=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
+
+# Execute the python script
+python3 /work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/tc_irad_multi_cleaned_SL.py "$Nom_dossier" "$Annee"
