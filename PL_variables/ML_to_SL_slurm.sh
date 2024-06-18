b47647580430d2fc27df5e58917cb3ed484ec620
Fabien <fabien.augsburger@unil.ch>
2024-06-18 13:33:17 +0200

    Ok turns out, I need to be careful with my config files ! rel and vert done for good

--- PL_variables/ML_to_SL_slurm.sh
+++ PL_variables/ML_to_SL_slurm.sh
@@ -12,7 +12,7 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 64G
 #SBATCH --time 02:00:00
-#SBATCH --array=1-42
+#SBATCH --array=1-336
 
 # Set your environment 1-609
 module purge
@@ -22,7 +22,7 @@ source ~/.bashrc
 conda activate kera_lgbm
 
 # Specify the path to the config file
-config=/work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/PL_variables/config_ML_to_SL_vert_2011.txt
+config=/work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/PL_variables/config_ML_to_SL_rel_missing.txt
 # echo "SLURM_ARRAY_TASK_ID is :${SLURM_ARRAY_TASK_ID}" >> /work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/case_study/output_test_all.txt
 
 # Extract the nom_var for the current $SLURM_ARRAY_TASK_ID
