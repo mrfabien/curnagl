26b18a045fffa698095301ea11131e6d4b0205d8
Fabien <fabien.augsburger@unil.ch>
2024-06-18 11:32:38 +0200

    vert_velocity 3 hours

--- PL_variables/run_tc_irad_cleaned_PL_v3_all_3h.sh
+++ PL_variables/run_tc_irad_cleaned_PL_v3_all_3h.sh
@@ -22,7 +22,7 @@ source ~/.bashrc
 conda activate kera_lgbm
 
 # Specify the path to the config file
-config=/work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/PL_variables/config_ML_to_SL_rel_2011.txt
+config=/work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/PL_variables/config_ML_to_SL_vert_2011.txt
 # echo "SLURM_ARRAY_TASK_ID is :${SLURM_ARRAY_TASK_ID}" >> /work/FAC/FGSE/IDYST/tbeucler/default/fabien/repos/curnagl/case_study/output_test_all.txt
 
 # Extract the nom_var for the current $SLURM_ARRAY_TASK_ID
